99fe3a642b5953884d1590ff7f801ae5d7c18a50
kt22rocks <chrislam5148@gmail.com>
2014-06-15 12:17:27 -0700

    pythonroom update

--- turtle2.py
+++ turtle2.py
@@ -3,15 +3,15 @@ import turtle
 jake = turtle.Turtle()
 angle=99
 List = ["green","red","magenta","blue","purple","maroon"]
-Listnum =  range(1,50)
+Listnum =  range(1,300)
 for color in List:
 	jake.color(color)
-	jake.forward(92)
+	jake.forward(89)
 	jake.left(angle)
 for number in Listnum:
 	for color in List:
 		jake.color(color)
-		jake.forward(89)
+		jake.forward(120)
 		jake.left(angle)
 
 
